74b979579c819409b46e4e44a80c5d50d9f4cad0
cnrpman <thinkinginjavadawn@gmail.com>
2017-11-13 15:55:58 +0800

    Add platform types

--- counter.py
+++ counter.py
@@ -7,6 +7,17 @@ res_list = ["cpu", "gres/gpu"]
 pattern_strings = ["{}=(\d+)".format(res) for res in res_list]
 patterns = [re.compile(pattern_string) for pattern_string in pattern_strings]
 
+# Platform type
+plat_avail = ['Bigvideo', 'P100', 'TITANXP', 'GTX1080']
+plat_tmp = ['Test']
+def plat_classify(platform):
+    if platform in plat_avail:
+        return 'w/'
+    elif platform in plat_tmp:
+        return 'tmp'
+    else:
+        return 'w/o'
+
 def main():
     with open('output.txt', 'r') as f:
         for line in f:
@@ -21,11 +32,11 @@ def main():
                 the_list.append(int(the_ele))
 
             # edit here to add more regist dict
-            regist_dict(name_dict, name, the_list)
-            regist_dict(platform_dict, platform, the_list)
+            regist_dict(name_dict, "[{:<3}] {}".format(plat_classify(platform), name), the_list)
+            regist_dict(platform_dict, "[{:<3}] {}".format(plat_classify(platform), platform), the_list)
     
     # edit here to add more output dict
-    output_dict(name_dict, "Aggregate by User Name")
+    output_dict(name_dict, "Aggregate by User Name and platform type")
     print
     output_dict(platform_dict, "Aggregate by Platform")
 
@@ -34,8 +45,8 @@ def list_str(the_list):
 
 def output_dict(the_dict, annotate):
     print("{}, output in order of {} and job".format(annotate, list_str(res_list)))
-    for key, value in the_dict.iteritems():
-        print("{:>15}:{}".format(key, list_str(value)))
+    for key, value in sorted(the_dict.iteritems()):
+        print("{:<22}{}".format(key, list_str(value)))
 
 content_len = len(res_list) + 1 # content + job
 def default_caller():
--- readme.md
+++ readme.md
@@ -1,7 +1,8 @@
 A sh script to output each user's hardware resource allocation on slurm.
 
 ## Updates ##
-* **v0.3** Add python script for grouping
+* **v0.35** Divide platforms into [w/], [w/o] and [tmp], represent different permission types of platforms
+* **v0.3** Add python script for grouping and sum
 * **v0.2** Jobs with 'PD' status are filtered
 * **v0.1** Initial version
 ## Usage ##
